4e04bcf0efd139b73f438c7976ebb0d35399b5eb
root <singh.babber@gmail.com>
2019-07-02 12:18:43 -0400

    Initial Version

---
+++ recipes/server.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+package 'apache2' do
+	action :install
+end
+
+
+file '/var/www/html/index.html' do
+	content '
+	<h1> Hello Balkar </h1>'
+end
+
+
+service 'apache2' do
+	action [:enable, :start]
+end
+
